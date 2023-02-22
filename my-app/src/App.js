0cb0f230ba075b59aece655e65832a33d398e9db
SeongJun KIM <sjkymy@gmail.com>
2023-02-22 17:31:25 +0900

    로그인 상태에 맞는 화면 노출하기

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,22 +2,24 @@ import Home from "./pages/Home/Home";
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
 import Nav from "./components/Nav";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import useAuthContext from "./hooks/useAuthContext";
 
 function App() {
-  const { isAuthReady } = useAuthContext();
+  const { isAuthReady, user } = useAuthContext();
   return (
     <div className="App">
       {isAuthReady ? (
         <BrowserRouter>
-        <Nav/>
-        <Routes>
-          <Route path="/" element={<Home/>}></Route>
-          <Route path="/login" element={<Login/>}></Route>
-          <Route path="/signup" element={<Signup/>}></Route>
-        </Routes>
-      </BrowserRouter>
+          <Nav/>
+          <Routes>
+            {/* 로그인 되있으면 홈으로, 아니라면 로그인 화면으로 이동합니다. */}
+            <Route path="/" element={user ? <Home /> : <Navigate replace={true} to="/login" />}></Route>
+            {/* 로그인이 되어있다면 로그인 화면이나 회원가입 화면으로 가지 못하게 만듭니다. */}
+            <Route path="/login" element={!user ? <Login /> : <Navigate to="/" replace={true} />}></Route>
+            <Route path="/signup" element={!user ? <Signup /> : <Navigate to="/" replace={true} />}></Route>
+          </Routes>
+        </BrowserRouter>
       ) : "loading..."}
     </div>
   );
